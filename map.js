6f1f98dbb6c8f209783c7811925c5bfd851aa727
FakeLucasNagy <bnlu24vx@student.ju.se>
2024-11-27 21:27:20 +0100

    Create map.js

---
+++ map.js
@@ -0,0 +1,22 @@
+export default class Map {
+    constructor() {
+        // set images
+    }
+    load() {
+        // preload images
+    }
+
+
+
+
+    
+    lvl1() {
+        // draw map
+    }
+    lvl2() {
+        // draw map
+    }
+    lvl3() {
+        // draw map
+    }
+}
