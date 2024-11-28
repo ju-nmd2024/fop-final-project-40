a5d374922f317664aa5894334f87337eadd4393f
FakeLucasNagy <bnlu24vx@student.ju.se>
2024-11-28 17:37:29 +0100

    removed unnecessary code

--- entities/bandage.js
+++ entities/bandage.js
@@ -6,34 +6,16 @@ export default class Bandage extends Entity {
     this.x = x;
     this.y = y;
     this.size = 8;  
-    this.bandages = [];
   }
-  setup(){
-
-    for( let i = bandages.length - 1; i>= 0; i--){
-      bandages[i].draw();
-      }
-    }
-
+  
   draw(){
     push();
     fill(255,0,0);
     ellipse(this.x,this.y, this.size, this.size);
     pop();
-  }
+  }    
 
-  healedBy(bandages) {
-    for (let bandage of bandages) {
-      let distance = dist(this.x, this.y, bandage.x, bandage.y);
-      if (distance < 16 && this.hp > 0) {
-        this.hp += 10;
-      }
-    }
-  }      
-
-
-  collisionWith(player, bandages){
-    
+  collisionWith(player, bandages){ 
       if (dist(player.x, player.y, this.x, this.y < this.size)) {
         player.hp += 10;
         bandages.splice(bandages.indexOf(this), 1);
